e8cac5312857f4336e5371484c91eb3c63728606
Slava <avs-spider@ya.ru>
2024-01-14 03:40:11 +0300

    Initial commit

---
+++ NewsPaper/news/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from .models import Post
+from .models import Post, Category
+from datetime import date
+
+
+class PostForm(forms.ModelForm):
+    #description = forms.CharField(min_length=20)
+
+    category = forms.ModelMultipleChoiceField(queryset=Category.objects.all(), widget=forms.CheckboxSelectMultiple(attrs={'category': 'category'}), label = 'Категория')
+    class Meta:
+        model = Post
+        fields = ['title', 'text', 'author']
+        fields = ['title', 'text', 'author', 'category']
+
+    """ def clean(self):
+        cleaned_data = super().clean()
+        description = cleaned_data.get("description")
+        name = cleaned_data.get("name")
+        if name == description:
+            raise ValidationError(
+                "Описание не должно быть идентично названию."
+            )
+        return cleaned_data """
---
+++ NewsPaper/news/views.py
@@ -0,0 +1,124 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post, Category
+from .filters import PostFilter
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+from datetime import date
+
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = 'index.html'
+
+
+
+class PostsList(LoginRequiredMixin, ListView):
+    model = Post
+    ordering = '-time'
+    template_name = 'post.html'
+    context_object_name = 'posts'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())
+        context['is_not_author'] = not self.request.user.groups.filter(name='authors').exists()
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'news'
+
+
+class PostSearch(ListView):
+    model = Post
+    ordering = '-time'
+    template_name = 'search.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        # Возвращаем из функции отфильтрованный список товаров
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+
+class PostCreate(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    permission_required = ('post.add_post',)
+    form_class = PostForm
+    model = Post
+    template_name = 'create_post.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        author_posts_today = Post.objects.filter(time__date=date.today(), author=post.author).count()
+        if author_posts_today == 3:
+            return render(self.request, template_name='post_limit.html', context={'author': post.author})
+        else:
+            if '/article/' in self.request.path:
+                type_ = 'AR'
+            elif '/news/' in self.request.path:
+                type_ = 'NE'
+            self.object.type = type_
+            return super().form_valid(form)
+
+
+class PostUpdate(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    permission_required = ('post.change_post',)
+    form_class = PostForm
+    model = Post
+    template_name = 'create_post.html'
+
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('post_list')
+
+
+class CategoryListView(PostsList):
+    model = Post
+    template_name = 'category_list.html'
+    context_object_name = 'category_news_list'
+
+
+    def get_queryset(self):
+        self.category = get_object_or_404(Category, id=self.kwargs['pk'])
+        queryset = Post.objects.filter(category=self.category).order_by('-time')
+        return queryset
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_not_subscriber'] = self.request.user not in self.category.subscribers.all()
+        context['category'] = self.category
+        return context
+
+
+@login_required
+def subscribe(request, pk):
+    user = request.user
+    category = Category.objects.get(id=pk)
+    category.subscribers.add(user)
+    message = 'Вы успешно подписались на рассылку новостей категории'
+    return  render(request, 'subscribe.html', {'category': category, 'message' : message})
+
+
+
